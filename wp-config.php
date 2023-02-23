5ae7a004f0421a8a733094b5a64d558594200e51
Tomasz Kaźmierczak <tomek@tusonic.pl>
2023-02-23 23:02:46 +0100

    0.2

--- wp-config.php
+++ wp-config.php
@@ -1,9 +1,15 @@
 <?php
+// Wylaczenie edytora motywów i wtyczek
+// Disable the Theme Editor and the Plugin Editor.
+define( 'DISALLOW_FILE_EDIT', true );
+define( 'DISALLOW_FILE_MODS', true );
 
 // In wp-config.php file locate:
+// W wp-config znajdz plik
 define('WP_DEBUG', false);
 
 // And change it to:
+// I zamien na to:
 define('WP_DEBUG', false);
 
 if ( ! WP_DEBUG ) {
