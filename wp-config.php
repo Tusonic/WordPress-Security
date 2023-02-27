f0b49ec38dc1b27873c6a27779115a0a9a14ed05
Tomasz Kaźmierczak <tomek@tusonic.pl>
2023-02-27 23:22:32 +0100

    0.3

--- wp-config.php
+++ wp-config.php
@@ -16,9 +16,19 @@ if ( ! WP_DEBUG ) {
 	ini_set('display_errors', 0);
 }
 
+// HIDE ERROR php.ini
+// Ukrycie błedów dla php.ini
+
+error_reporting(0);
+ini_set('display_errors','Off');
+ini_set('error_reporting',E_ALL);
+
 /* public_html/wp.config.php
 ########## START ########## */
 
+error_reporting(0);
+ini_set('display_errors','Off');
+ini_set('error_reporting',E_ALL);
 define( 'DISALLOW_FILE_EDIT', true );
 define( 'DISALLOW_FILE_MODS', true );
 define('WP_DEBUG', false);
